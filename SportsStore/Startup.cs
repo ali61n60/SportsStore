a7b734168d7badb089347202eef3b766abe25861
Ali <ali62n62@yahoo.com>
2017-07-14 19:56:48 +0430

    test project added

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -23,7 +23,10 @@ namespace SportsStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
---
+++ Test/SportsStore.Tests/Test1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SportsStore.Tests
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void t1()
+        {
+            Assert.That(true,Is.False);
+        }
+    }
+}
